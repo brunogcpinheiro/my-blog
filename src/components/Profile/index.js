b2ecbb41d952335c384250667c03c86576c43f60
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2020-01-16 19:32:05 +0000

    creating the profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="profile-wrapper">
+    <h1>Bruno Carpinelli</h1>
+    <h2>Full Stack Developer</h2>
+    <p>Apassionate about Technology! Creating amazing stuffs with JavaScript. 
+      (Node.JS, React, React Native, Vue.JS, GraphQL and much more!)
+    </p>
+  </div>   
+)
+
+export default Profile
