283b5606f39fce173e5da32b4bd2c736d47c8cf3
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2020-01-23 15:04:17 +0000

    finish page transition w/ image

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,24 @@
 import React from "react"
 
 import * as S from "./styles"
+import Background from "../../images/bg.jpg"
 
 const active = {
   borderBottom: "2px solid #999"
+}
+
+const animation = {
+  cover: true,
+  duration: 2,
+  bg: `
+    url(${Background})
+    center / cover
+    no-repeat   
+    fixed
+    padding-box
+    content-box
+    white
+  `
 }
 
 const Navigation = () => {
@@ -14,19 +29,19 @@ const Navigation = () => {
       </S.Logo>
       <S.Menu>
         <li>
-          <S.MenuLink cover to="/" activeStyle={active}>Início</S.MenuLink>
+          <S.MenuLink to="/" activeStyle={active} {...animation}>Início</S.MenuLink>
         </li>
         <li>
-          <S.MenuLink cover to="/sobre" activeStyle={active}>Sobre mim</S.MenuLink>
+          <S.MenuLink to="/sobre" activeStyle={active} {...animation}>Sobre mim</S.MenuLink>
         </li>
         <li>
-          <S.MenuLink cover to="/projetos" activeStyle={active}>Projetos</S.MenuLink>
+          <S.MenuLink to="/projetos" activeStyle={active} {...animation}>Projetos</S.MenuLink>
         </li>
         <li>
-          <S.MenuLink cover to="/habilidades" activeStyle={active}>Habilidades</S.MenuLink>
+          <S.MenuLink to="/habilidades" activeStyle={active} {...animation}>Habilidades</S.MenuLink>
         </li>
         <li>
-          <S.MenuLink cover to="/contato" activeStyle={active}>Contato</S.MenuLink>
+          <S.MenuLink to="/contato" activeStyle={active} {...animation}>Contato</S.MenuLink>
         </li>
       </S.Menu>
     </S.NavigationWrapper>
