f1aaf9ccfe428ed98510c049c9bd4a3eeecd11d9
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2020-01-24 15:18:23 +0000

    fix footer padding

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const FooterWrapper = styled.footer`
   display: flex;
   justify-content: flex-end;
-  padding: 10px 30px;
+  padding: 20px 30px 5px 30px;
   width: 100%;
   background: #f5f5f5;
 `
